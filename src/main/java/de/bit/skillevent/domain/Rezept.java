0c0d0dce2b6a5b5741946df03751c1a41353fe2d
Philipp Bayer <p.bayer@freemail.hu>
2015-04-30 14:40:44 +0200

    formatting

--- src/main/java/de/bit/skillevent/domain/Rezept.java
+++ src/main/java/de/bit/skillevent/domain/Rezept.java
@@ -1,32 +1,36 @@
 package de.bit.skillevent.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Rezept  extends BasisDomainObject{
-	private String pizzaId;
-	private List<String> zutatIds;
+public class Rezept extends BasisDomainObject {
 
-	public Rezept(String pizzaId){
-		this.pizzaId = pizzaId;
-	}
-	
-	public Rezept addIngredient(String zutatId){
-		zutatIds.add(zutatId);
-		return this;
-	}
-	public String getPizzaId() {
-		return pizzaId;
-	}
+    private String pizzaId;
 
-	public void setPizzaId(String pizzaId) {
-		this.pizzaId = pizzaId;
-	}
+    private List<String> zutatIds = new ArrayList<>();
 
-	public List<String> getZutatIds() {
-		return zutatIds;
-	}
+    public Rezept(String pizzaId) {
+        this.pizzaId = pizzaId;
+    }
 
-	public void setZutatIds(List<String> zutatIds) {
-		this.zutatIds = zutatIds;
-	}
+    public Rezept addIngredient(String zutatId) {
+        zutatIds.add(zutatId);
+        return this;
+    }
+
+    public String getPizzaId() {
+        return pizzaId;
+    }
+
+    public void setPizzaId(String pizzaId) {
+        this.pizzaId = pizzaId;
+    }
+
+    public List<String> getZutatIds() {
+        return zutatIds;
+    }
+
+    public void setZutatIds(List<String> zutatIds) {
+        this.zutatIds = zutatIds;
+    }
 }
