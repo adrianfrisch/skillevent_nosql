0a4d500e0d610b5bdc7a4582bc9fdbb52447521a
Philipp Bayer <p.bayer@freemail.hu>
2015-04-30 14:40:20 +0200

    export to FS

--- src/main/java/de/bit/skillevent/domain/exp/DomainExporter.java
+++ src/main/java/de/bit/skillevent/domain/exp/DomainExporter.java
@@ -1,51 +1,77 @@
 package de.bit.skillevent.domain.exp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import de.bit.skillevent.domain.*;
+import org.fluttercode.datafactory.impl.DataFactory;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import de.bit.skillevent.domain.Bestellung;
-import de.bit.skillevent.domain.Kunde;
-import de.bit.skillevent.domain.Pizza;
-import de.bit.skillevent.domain.Rezept;
-import de.bit.skillevent.domain.Zutat;
-
 public class DomainExporter {
-	public static void main(String [] args) throws Exception{
-		List<Zutat> ingredients = new ArrayList<Zutat>();
-		ingredients.add(new Zutat("Salami", 1.0 , false));
-		ingredients.add(new Zutat("Pepperoniwurst", 1.0 , false));
-		ingredients.add(new Zutat("Schinken", 1.0 , false));
-		ingredients.add(new Zutat("Pilze", 1.0 , false));
-		ingredients.add(new Zutat("Mozzarella", 1.0 , false));
-		ingredients.add(new Zutat("Provolone", 1.0 , false));
-		ingredients.add(new Zutat("Gouda", 1.0 , false));
-		ingredients.add(new Zutat("Edamer", 1.0 , false));
-		ingredients.add(new Zutat("Parmesan", 1.0 , false));
-		ingredients.add(new Zutat("Basilikum", 1.0 , false));
-		ingredients.add(new Zutat("Oregano", 1.0 , false));
-		
-		List<Pizza> pizzas = new ArrayList<Pizza>();
-		pizzas.add(new Pizza("Schinken"));
-		pizzas.add(new Pizza("Salami"));
-		pizzas.add(new Pizza("4 Stragioni"));
-		pizzas.add(new Pizza("4 Formaggi"));
-		
-		List<Rezept> recipies = new ArrayList<Rezept>();
-		recipies.add(new Rezept("schinken").addIngredient("schinken").addIngredient("gouda"));
-		
-		List<Kunde> customers = new ArrayList<Kunde>();
-		customers.add(new Kunde("Adrian", "Frischauf", false));
-		customers.add(new Kunde("Philipp", "Bayer", false));
-		customers.add(new Kunde("Anton", "Obermayer", true));
-		
-		List<Bestellung> orders = new ArrayList<Bestellung>();
-		
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.writeValue(System.out, ingredients);
-	}
+
+    public static final int VEG_PROB = 30;
+
+    private static ObjectWriter objectWriter;
+
+    public static void main(String[] args) throws Exception {
+
+        DataFactory dataFactory = new DataFactory();
+
+        List<Zutat> ingredients = new ArrayList<>();
+        ingredients.add(new Zutat("Salami", 1.0, false));
+        ingredients.add(new Zutat("Pepperoniwurst", 1.0, false));
+        ingredients.add(new Zutat("Schinken", 1.0, false));
+        ingredients.add(new Zutat("Pilze", 1.0, false));
+        ingredients.add(new Zutat("Mozzarella", 1.0, false));
+        ingredients.add(new Zutat("Provolone", 1.0, false));
+        ingredients.add(new Zutat("Gouda", 1.0, false));
+        ingredients.add(new Zutat("Edamer", 1.0, false));
+        ingredients.add(new Zutat("Parmesan", 1.0, false));
+        ingredients.add(new Zutat("Basilikum", 1.0, false));
+        ingredients.add(new Zutat("Oregano", 1.0, false));
+
+        List<Pizza> pizzas = new ArrayList<>();
+        pizzas.add(new Pizza("Schinken"));
+        pizzas.add(new Pizza("Salami"));
+        pizzas.add(new Pizza("4 Stragioni"));
+        pizzas.add(new Pizza("4 Formaggi"));
+
+        List<Rezept> recipies = new ArrayList<>();
+        recipies.add(new Rezept("schinken").addIngredient("schinken").addIngredient("gouda"));
+
+        List<Kunde> customers = new ArrayList<>();
+        int i = 0;
+        while (i < 1000) {
+            customers.add(new Kunde(dataFactory.getFirstName(), dataFactory.getLastName(), dataFactory.chance(VEG_PROB)));
+            i++;
+        }
+
+        List<Bestellung> orders = new ArrayList<>();
+
+        objectWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();
+
+        exportToJsonFile(ingredients, "ingredients.json");
+        exportToJsonFile(customers, "customers.json");
+
+    }
+
+    private static void exportToJsonFile(Object data, String fileName) throws IOException {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            objectWriter.writeValue(baos, data);
+            writeToFile(fileName, baos);
+        }
+    }
+
+    private static void writeToFile(String fileName, ByteArrayOutputStream baos) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get("src/main/resources/" + fileName), StandardCharsets.UTF_8)) {
+            writer.write(baos.toString(StandardCharsets.UTF_8.name()));
+        }
+    }
 }
