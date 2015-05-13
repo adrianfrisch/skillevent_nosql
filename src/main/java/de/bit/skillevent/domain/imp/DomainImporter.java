6fe0a2e385af8222b317a54268af918be6ff58aa
Philipp Bayer <p.bayer@freemail.hu>
2015-05-13 23:44:32 +0200

    ups

--- src/main/java/de/bit/skillevent/domain/imp/DomainImporter.java
+++ src/main/java/de/bit/skillevent/domain/imp/DomainImporter.java
@@ -1,8 +1,6 @@
 package de.bit.skillevent.domain.imp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.bit.skillevent.domain.Kunde;
-import de.bit.skillevent.domain.Pizza;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,10 +12,6 @@ import java.util.List;
 public class DomainImporter {
 
     private static ObjectMapper mapper = new ObjectMapper();
-
-    public static void main(String[] args) throws Exception {
-        readData();
-    }
 
     public static <T> List<T> readData(String fileName, Class<T> c) throws IOException {
         List data = mapper.readValue(new File("src/main/resources/" + fileName), mapper.getTypeFactory().constructCollectionType(List.class, c));
